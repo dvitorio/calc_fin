0e2f4a348485be010395acb461cf2f2f978431fb
Daniel Vitorio <dvitorio2@gmail.com>
2022-07-17 23:37:41 -0300

    Add functions 'amount', 'rate', 'time' and 'capital'

--- js/compoundRate.js
+++ js/compoundRate.js
@@ -1,11 +1,17 @@
-const title = document.querySelector('.imageTitle');
+/**MOSTRA TITULO DO APP */
+function showTitle(){
+     const title = document.querySelector('.imageTitle');
+     title.classList.remove('d-none');
+}
+
 
 /** ABRE TELA DE MONTANTE*/
 function openAmount(){
-     closeCapital();
-     closeRate();
      const openAmount = document.querySelector("#compoundAmount");
      openAmount.classList.remove('d-none');
+     closeCapital();
+     closeRate();
+     closeTime();
 }
 
 /**CALCULAR MONTANTE COMPOSTO */
@@ -13,17 +19,17 @@ function calculateAmountCompound(){
      const capitalAmount = parseFloat(document.getElementById("capitalAmount").value.replace(",","."));
      const timeAmount = parseFloat(document.getElementById("timeAmount").value.replace(",","."));
      const rateAmount = parseFloat(document.getElementById("rateAmount").value.replace(",","."));
-          if((capitalAmount > 0) && (timeAmount > 0) && (rateAmount > 0)){
-               amountAmount = capitalAmount * Math.pow((1 + rateAmount/100), timeAmount);
-               document.getElementById("inputAmountResult").value = "R$ " + amountAmount.toFixed(2);
-               const btnResult = document.getElementById("btnAmountResult");
-               btnResult.classList.remove('d-none');
-               const inputAmountResult = document.getElementById("inputAmountResult");
-               inputAmountResult.classList.remove('d-none');
-          }
-          else{
-               alert("Capital, tempo e/ou taxa deve(m) ser valor(es) positivos!");
-          }
+          amountAmount = capitalAmount * Math.pow((1 + rateAmount/100), timeAmount);
+          interestAmount = amountAmount - capitalAmount;
+     document.getElementById("inputAmountResult").value = "R$ " + amountAmount.toFixed(2);
+     document.getElementById("inputInterestResult").value = "R$ " + interestAmount.toFixed(2);
+     const amountResult = document.getElementById("amountResult");
+     amountResult.classList.remove('d-none');
+     inputInterestResult.classList.remove('d-none');
+     const inputAmountResult = document.getElementById("inputAmountResult");
+     inputAmountResult.classList.remove('d-none');
+     const btnAmountResult = document.getElementById("btnAmountResult");
+     btnAmountResult.classList.remove('d-none');
 }
 
 /**LIMPA OS CAMPOS DO MONTANTE COMPOSTO */
@@ -32,25 +38,31 @@ function cleanAmount(){
      document.getElementById("timeAmount").value = "";
      document.getElementById("rateAmount").value = "";
      document.getElementById("inputAmountResult").value = "";
+     document.getElementById("btnAmountResult").value = "";
+     document.getElementById("inputInterestResult").value = "";
 }
 
 /**FECHA TELA DE MONTANTE */
 function closeAmount(){
      cleanAmount();
-     const closeAmount = document.querySelector("#compoundAmount");
+     const closeAmount = document.getElementById("compoundAmount");
      closeAmount.classList.add('d-none');
-     const inputAmountResult = document.getElementById("inputAmountResult");
+     const inputAmountResult = document.querySelector("#inputAmountResult");
      const btnAmountResult = document.getElementById("btnAmountResult");
      inputAmountResult.classList.add('d-none');
      btnAmountResult.classList.add('d-none');
+     interestAmount.classList.add('d-none');
 }
+
+/**========================= CÁLCULO DO CAPITAL EM JUROS COMPOSTOS =====================================*/
 
 /**ABRE TELA DE CAPITAL */
 function openCapital(){
+     const openCapital = document.getElementById("compoundCapital");
+     openCapital.classList.remove('d-none');
      closeAmount();
      closeRate();
-     const openCapital = document.querySelector("#compoundCapital");
-     openCapital.classList.remove('d-none');
+     closeTime();
 }
 
 /**CALCULAR CAPITAL COMPOSTO */
@@ -74,12 +86,6 @@ function cleanCapital(){
      document.getElementById("inputCapitalResult").value = "";
 }
 
-/**FECHA TELA DE CAPITAL */
-function closeCapital(){
-     const closeCapital = document.querySelector("#compoundCapital");
-     closeCapital.classList.add('d-none');
-}
-
 /**FECHA TELA DE CAPITAL COMPOSTO */
 function closeCapital(){
      cleanCapital();
@@ -89,12 +95,15 @@ function closeCapital(){
      compoundCapital.classList.add('d-none');
 }
 
+/**========================= CÁLCULO DA TAXA DE JUROS COMPOSTA =====================================*/
+
 /**ABRE TELA DE TAXA */
 function openRate(){
-     closeAmount();
-     closeCapital();
      const openRate = document.getElementById("compoundRate");
      openRate.classList.remove('d-none'); 
+     closeAmount();
+     closeTime();
+     closeCapital();
 }
 
 /**CALCULA A TAXA COMPOSTA */
@@ -127,10 +136,53 @@ function closeRate(){
      compoundRate.classList.add('d-none');
 }
 
-/**FECHA TELA DE TAXA COMPOSTA*/
-function closeRate(){
-     const closeRate = document.getElementById("compoundRate");
-     closeRate.classList.add('d-none'); 
+/**========================= CÁLCULO DO TEMPO (PERÍODO) EM JUROS COMPOSTOS =====================================*/
+
+/**ABRE TELA DE TEMPO COMPOSTO*/
+function openTime(){
+     const openTime = document.getElementById("compoundTime");
+     openTime.classList.remove('d-none'); 
+     closeRate();
+     closeCapital();
+     closeAmount();
 }
+
+/**CALCULA O TEMPO COMPOSTO */
+function calculateTimeCompound(){
+     const capitalTime = parseFloat(document.getElementById("capitalTime").value.replace(",","."));
+     const rateTime = parseFloat(document.getElementById("rateTime").value.replace(",","."));
+     const amountTime = parseFloat(document.getElementById("amountTime").value.replace(",","."));
+          if(amountTime >= capitalTime){
+               timeTime = (Math.log(amountTime) - Math.log(capitalTime)) / Math.log(1 + rateTime/100);
+               document.getElementById("inputTimeResult").value = timeTime.toFixed(0) + " meses";
+               const btnTimeResult = document.getElementById("btnTimeResult");
+               btnTimeResult.classList.remove('d-none');
+               const inputTimeResult = document.getElementById("inputTimeResult");
+               inputTimeResult.classList.remove('d-none');
+          }
+          else{
+               alert("Montante inválido! O valor do montante não pode ser inferior ao valor do capital!");
+          }
+}
+
+/**LIMPA OS CAMPOS DO TEMPO COMPOSTO */
+function cleanTime(){
+     document.getElementById("capitalTime").value = "";
+     document.getElementById("rateTime").value = "";
+     document.getElementById("amountTime").value = "";
+     document.getElementById("inputTimeResult").value = "";
+}
+
+/**FECHA TELA DE TEMPO COMPOSTO */
+function closeTime(){
+     cleanTime();
+     btnTimeResult.classList.add('d-none');
+     const closeTime = document.getElementById("compoundTime");
+     closeTime.classList.add('d-none'); 
+}
+
+
+
+
 
 
