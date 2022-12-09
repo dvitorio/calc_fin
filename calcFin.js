68fca698d78db824e7da10b460fbb38095400643
Daniel Vitorio <dvitorio2@gmail.com>
2022-12-08 22:36:49 -0300

    Construção e implementação do modal de taxa de juros simples

--- calcFin.js
+++ calcFin.js
@@ -3,47 +3,53 @@ const amountResult = document.getElementById('amountResult');
 const btnCalculateSimpleAmount = document.getElementById('btnCalculateSimpleAmount');
 const btnCleanSimpleAmount = document.getElementById('btnCleanSimpleAmount');
 const btnCleanSimpleCapital = document.getElementById('btnCleanSimpleCapital');
+const btnCleanSimpleRate = document.getElementById('btnCleanSimpleRate');
 const btnCloseApp = document.getElementById('btnCloseApp');
 const btnCloseMenuSimple = document.getElementById('btnCloseMenuSimple');
 const btnCloseSimpleAmount = document.getElementById('btnCloseSimpleAmount');
 const btnCloseSimpleCapital = document.getElementById('btnCloseSimpleCapital');
+const btnCloseSimpleRate = document.getElementById('btnCloseSimpleRate');
 const btnJComp = document.getElementById('btnJComp');
 const btnJSimp = document.getElementById('btnJSimp');
 const btnSimpleAmount = document.getElementById('btnSimpleAmount');
 const btnSimpleCapital = document.getElementById('btnSimpleCapital');
+const btnSimpleRate = document.getElementById('btnSimpleRate');
 const btnSimpleTime = document.getElementById('btnSimpleTime');
 const capitalResult = document.getElementById('capitalResult');
 const feesResult = document.getElementById('feesResult');
 const menu = document.getElementById('menu');
 const menuButtons = document.getElementById('menuButtons');
+const rateResult = document.getElementById('rateResult');
 const simpleAmountModal = document.getElementById('simpleAmountModal');
 const simpleAmountResult = document.getElementById('simpleAmountResult');
 const simpleCalculateAmount = document.getElementById('simpleCalculateAmount');
 const simpleCalculateCapital = document.getElementById('simpleCalculateCapital');
 const simpleCapitalModal = document.getElementById('simpleCapitalModal');
 const simpleCapitalResult = document.getElementById('simpleCapitalResult');
+const simpleRateModal = document.getElementById('simpleRateModal');
+const simpleRateResult = document.getElementById('simpleRateResult');
 
 
 
 //Fecha o aplicativo
-btnCloseApp.onclick = () => {
+btnCloseApp.onclick = ()=> {
      window.close();
 }
 
 //Abre menu geral
-btnJSimp.onclick = () => {
+btnJSimp.onclick = ()=> {
      menu.classList.add('d-none');
      menuButtons.classList.remove('d-none');
 }
 
 //Fecha menu geral e abre menu de montante de juros simples
-btnSimpleAmount.onclick = () => {
+btnSimpleAmount.onclick = ()=> {
      menuButtons.classList.add('d-none');
      simpleAmountModal.classList.remove('d-none');
 }
 
 //Calcula montante simples
-simpleCalculateAmount.onclick = () => {
+simpleCalculateAmount.onclick = ()=> {
      if ((simpleCapital.value != '' && simpleCapital.value > 0) && (simpleTime.value != '' && simpleTime.value > 0) && (simpleRate.value != '' && simpleRate.value > 0)) {
           let simpleCapital = parseFloat(document.getElementById('simpleCapital').value.replace(".",","));
           let simpleTime = parseFloat(document.getElementById('simpleTime').value.replace(".",","));
@@ -60,14 +66,14 @@ simpleCalculateAmount.onclick = () => {
 }
 
 //Fecha o menu de montante de juros simples
-btnCloseSimpleAmount.onclick = () => {
+btnCloseSimpleAmount.onclick = ()=> {
      menu.classList.add('d-none');
      menuButtons.classList.remove('d-none');
      simpleAmountModal.classList.add('d-none');
 }
 
 //Volta para o menu geral
-btnCloseMenuSimple.onclick = () => {
+btnCloseMenuSimple.onclick = ()=> {
      menuButtons.classList.add('d-none');
      menu.classList.remove('d-none');
 }
@@ -105,6 +111,35 @@ btnCloseSimpleCapital.onclick = ()=>{
      menuButtons.classList.remove('d-none');
      simpleCapitalModal.classList.add('d-none');
 }
+
+//Abre a tela de taxa de juros simples
+btnSimpleRate.onclick = ()=>{
+     simpleRateModal.classList.remove('d-none');
+     menuButtons.classList.add('d-none')
+}
+
+//Calcula a taxa de juros simples
+simpleCalculateRate.onclick = ()=>{
+     let simpleCapitalRate = parseFloat(document.getElementById('simpleCapitalRate').value); 
+     let simpleTimeRate = parseFloat(document.getElementById('simpleTimeRate').value); 
+     let simpleAmountRate = parseFloat(document.getElementById('simpleAmountRate').value); 
+     simpleRateResult.value = ((simpleAmountRate - simpleCapitalRate)/(simpleCapitalRate * simpleTimeRate) * 100).toFixed(2);
+     rateResult.classList.remove('d-none');
+     simpleRateResult.classList.remove('d-none');
+     btnCleanSimpleRate.classList.remove('d-none');
+     btnCloseSimpleRate.classList.remove('d-none');
+}
+
+btnCleanSimpleRate.onclick = ()=>{
+     simpleCapitalRate.value = '';
+     simpleTimeRate.value = '';
+     simpleAmountRate.value = '';
+     simpleRateResult.value = '';
+}
+
+
+
+
 
 
 
