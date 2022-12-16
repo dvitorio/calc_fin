fec9f9d0259bd5a59bf79727da5024329f633677
Daniel Vitorio <dvitorio2@gmail.com>
2022-12-16 15:38:02 -0300

    Construção, implementação e validação dos campos do modal de tempo composto

--- js/calcFinComp.js
+++ js/calcFinComp.js
@@ -4,20 +4,36 @@ const amountCompResult = document.getElementById('amountCompResult');
 const amountFeesResult = document.getElementById('amountFeesResult');
 const btnCalculateCompAmount = document.getElementById('btnCalculateCompAmount');
 const btnCalculateCompCapital = document.getElementById('btnCalculateCompCapital');
+const btnCalculateCompRate = document.getElementById('btnCalculateCompRate');
+const btnCalculateCompTime = document.getElementById('btnCalculateCompTime');
 const btnCleanCompAmountResult = document.getElementById('btnCleanCompAmountResult');
 const btnCleanCompCapitalResult = document.getElementById('btnCleanCompCapitalResult');
+const btnCleanCompRateResult = document.getElementById('btnCleanCompRateResult');
+const btnCloseCompRateResult = document.getElementById('btnCloseCompRateResult');
+const btnCloseCompTimeResult = document.getElementById('btnCloseCompTimeResult');
 const btnCloseMenuComp = document.getElementById('btnCloseMenuComp');
 const btnCompAmount = document.getElementById('btnCompAmount');
 const btnCompAmountButtons = document.getElementById('btnCompAmountButtons');
 const btnCompCapital = document.getElementById('btnCompCapital');
 const btnCompCapitalButtons = document.getElementById('btnCompCapitalButtons');
+const btnCompRate = document.getElementById('btnCompRate');
+const btnCompRateButtons = document.getElementById('btnCompRateButtons');
+const btnCompTime = document.getElementById('btnCompTime');
+const btnCompTimeButtons = document.getElementById('btnCompTimeButtons');
 const btnJComp = document.getElementById('btnJComp');
 const capitalCompResult = document.getElementById('capitalCompResult');
 const compAmountCapital = document.getElementById('compAmountCapital');
 const compAmountModal = document.getElementById('compAmountModal');
 const compCapitalModal = document.getElementById('compCapitalModal');
 const compFeesAmountResult = document.getElementById('compFeesAmountResult');
+const compRate = document.getElementById('compRate');
+const compRateModal = document.getElementById('compRateModal');
+const compTimeModal = document.getElementById('compTimeModal');
+const compTimeRate = document.getElementById('compTimeRate');
 const menuButtonsComp = document.getElementById('menuButtonsComp');
+const rateCompResult = document.getElementById('rateCompResult');
+const timeCompResult = document.getElementById('timeCompResult');
+
 
 
 //Fecha o aplicativo
@@ -31,6 +47,22 @@ btnJComp.addEventListener('click', ()=>{
      menu.classList.add('d-none');
 });
 
+//Volta ao menu de opções compostas
+function backCompMenu(){
+     menuButtonsComp.classList.add('d-none');
+     compAmountModal.classList.add('d-none');
+     compCapitalModal.classList.add('d-none');
+     menu.classList.remove('d-none');
+}
+
+//Fechao menu de juros compostos
+btnCloseMenuComp.addEventListener('click', ()=>{
+     menu.classList.remove('d-none');
+     menuButtonsComp.classList.add('d-none');
+});
+
+
+/**========================================== MONTANTE COMPOSTO ================================= */
 //Acessa o modal de montante composto
 btnCompAmount.onclick = ()=>{
      compAmountModal.classList.remove('d-none');
@@ -58,13 +90,13 @@ btnCleanCompAmountResult.addEventListener('click', ()=>{
      amountComp.classList.add('d-none');
 })
 
-//Fecha o modal de juros compostos
+//Fecha o modal de montante composto
 btnCloseCompAmountResult.onclick = ()=>{
      compAmountModal.classList.add('d-none');
      menuButtonsComp.classList.remove('d-none');
-
 }
 
+/**======================================= CAPITAL COMPOSTO ===================================== */
 //Acessa o menu de capital composto
 btnCompCapital.onclick = ()=>{
      menuButtonsComp.classList.add('d-none');
@@ -102,20 +134,90 @@ btnCloseCompCapitalResult.addEventListener('click', ()=>{
      capitalCompResult.classList.add('d-none');
 });
 
-btnCloseMenuComp.addEventListener('click', ()=>{
-     menu.classList.remove('d-none');
+
+/** ======================================== TAXA COMPOSTA ====================================== */
+//Abre o modal de taxa composta
+btnCompRate.onclick = ()=>{
      menuButtonsComp.classList.add('d-none');
+     compRateModal.classList.remove('d-none');
+     compCapitalModal.classList.add('d-none');
+     compAmountModal.classList.add('d-none');
+     compTimeModal.classList.add('d-none');
+     rateCompResult.classList.add('d-none');
+     btnCompRateButtons.classList.add('d-none');
+}
+
+//Calcula a taxa composta
+btnCalculateCompRate.addEventListener('click', ()=>{
+     let compAmountRate = parseFloat(document.getElementById('compAmountRate').value);
+     let compCapitalRate = parseFloat(document.getElementById('compCapitalRate').value);
+     let compTimeRate = parseFloat(document.getElementById('compTimeRate').value);
+          if((compAmountRate > 0) && (compCapitalRate) && (compTimeRate > 0)){
+               rateComp.value = ((((compAmountRate/compCapitalRate) ** (1/compTimeRate)) - 1) * 100).toFixed(2);
+               rateCompResult.classList.remove('d-none');
+               rateComp.classList.remove('d-none'); 
+               btnCompRateButtons.classList.remove('d-none');
+          }
+          else{
+               alert('Montante, capital e o período precisam ser valores positivos!');
+          }
+});
+
+//Limpa os campos do modal de taxa composta
+btnCleanCompRateResult.onclick = ()=>{
+     document.getElementById('compAmountRate').value = '';
+     document.getElementById('compCapitalRate').value = '';
+     document.getElementById('compTimeRate').value = '';
+     document.getElementById('rateComp').value = '';
+}
+
+//Fecha o modal de taxa composta
+btnCloseCompRateResult.onclick = ()=>{
+     compRateModal.classList.add('d-none');
+     rateComp.classList.add('d-none');
+     rateCompResult.classList.add('d-none');
+     menuButtonsComp.classList.remove('d-none');
+}
+
+/**=================================================== PERÍODO COMPOSTO ========================================== */
+btnCompTime.onclick = ()=>{
+     compTimeModal.classList.add('d-none');
+     compCapitalModal.classList.add('d-none');
+     compRateModal.classList.add('d-none');
+     compAmountModal.classList.remove('d-none');
+     menuButtonsComp.classList.add('d-none');
+}
+
+//Calcula o período composto
+btnCalculateCompTime.addEventListener('click', ()=>{
+     let compAmountTime = parseFloat(document.getElementById('compAmountTime').value);
+     let compCapitalTime = parseFloat(document.getElementById('compCapitalTime').value);
+     let compRateTime = parseFloat(document.getElementById('compRateTime').value/100);
+     if((compAmountTime > 0) && (compCapitalTime > 0) && (compRateTime)){
+          if(compAmountTime > compCapitalTime){
+               timeComp.value = ((Math.log(compAmountTime) - Math.log(compCapitalTime))/Math.log(1 + compRateTime)).toFixed(4);
+               timeCompResult.classList.remove('d-none');
+               timeComp.classList.remove('d-none');
+               btnCompTimeButtons.classList.remove('d-none');
+          }
+          else{
+               alert('O montante não pode ser menor que o capital!');
+          }
+     }
+     else{
+          alert('Montante, capital e a taxa precisam ser valores positivos!');
+     }
 
 });
 
-
-//Volta ao menu de opções compostas
-function backCompMenu(){
-     menuButtonsComp.classList.add('d-none');
-     compAmountModal.classList.add('d-none');
+//Fecha o modal de período composto  volta ao menu de opções
+btnCloseCompTimeResult.addEventListener('click', ()=>{
+     compTimeModal.classList.add('d-none');
+     compRateModal.classList.add('d-none');
      compCapitalModal.classList.add('d-none');
-     menu.classList.remove('d-none');
-}
+     compAmountModal.classList.add('d-none');
+})
+
 
 
 
